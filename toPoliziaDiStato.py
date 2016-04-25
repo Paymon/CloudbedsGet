6a2a84dd6c7faafe50eda5887a5cd332f8620b61
Paymon <matteo.noris@gmail.com>
2016-04-25 09:26:28 +0200

    Nuovo Codice

--- toPoliziaDiStato.py
+++ toPoliziaDiStato.py
@@ -4,23 +4,37 @@ import csv
 
 file_TipoAlloggi = "Codici_TipoAlloggiato.csv"
 
-def TipoAlloggio(APIfield):
-    with open(file_TipoAlloggi) as csvfile:
+def get_csv(APIfield, file):
+#lunghezza 2 caratteri
+    with open(file) as csvfile:
         reader = csv.reader(csvfile, delimiter=':')
         for row in reader:
-            if row[0] == APIfield:
+            if row[0] == APIfield.upper():
                 return row[1]
 
-def DataArrivo(APIfield):
-    aaaa = APIfield[0:3]
-    mm = APIfield[5:6]
-    gg = APIfield[8:9]
-    return newdata = (gg + "/" + mm + "/" + aaaa)
+def Data(APIfield):
+#lunghezza 10 caratteri
+    aaaa = APIfield[0:4]
+    mm = APIfield[5:7]
+    gg = APIfield[8:10]
+    newdata = (gg + "/" + mm + "/" + aaaa)
+    return newdata
+
+def NGiorni(APIfield):
+#lunghezza 2 caratteri
+    gg = APIfield
+    return gg
+
+def Name(APIfield, length):
+    name = APIfield
+    for j in range(length - len(APIfield)):
+        name += "."
+    return name
 
 def lengthcheck(str):
     print(str)
     print(len(str))
 
 
-string = TipoAlloggio('FA')
+string = get_csv('fA', file_TipoAlloggi) + Data('2016/04/25') + NGiorni('01') + Name('Noris', 50) + Name('Matteo', 30) + 'm' + Data('1976/10/31')
 lengthcheck(string)
