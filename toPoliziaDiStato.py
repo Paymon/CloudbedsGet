10ac41f44aaca755d7b3d546db3a322c1c4dc353
Paymon <matteo.noris@gmail.com>
2016-04-27 12:03:39 +0200

    Nuovo Codice

--- toPoliziaDiStato.py
+++ toPoliziaDiStato.py
@@ -3,38 +3,63 @@ import csv
 
 
 file_TipoAlloggi = "Codici_TipoAlloggiato.csv"
+file_CodiceComune = "Comuni.csv"
+file_CodiceNazione = "Nazioni.csv"
+file_CodiceDocumento = "Documenti.csv"
 
-def get_csv(APIfield, file):
-#lunghezza 2 caratteri
+def get_csv(file, FieldIN, FieldOUT, APIfield):
     with open(file) as csvfile:
         reader = csv.reader(csvfile, delimiter=':')
         for row in reader:
-            if row[0] == APIfield.upper():
-                return row[1]
+            if row[FieldIN] == APIfield.upper():
+                return row[FieldOUT]
+
 
 def Data(APIfield):
-#lunghezza 10 caratteri
     aaaa = APIfield[0:4]
     mm = APIfield[5:7]
     gg = APIfield[8:10]
     newdata = (gg + "/" + mm + "/" + aaaa)
     return newdata
 
+
 def NGiorni(APIfield):
-#lunghezza 2 caratteri
     gg = APIfield
     return gg
+
 
 def Name(APIfield, length):
     name = APIfield
     for j in range(length - len(APIfield)):
-        name += "."
+        name += " "
     return name
+
+
+def CodComune(APIFieldNazione, APIFieldComune):
+    if APIFieldNazione.upper() == "ITALIA":
+        return get_csv(file_CodiceComune, 1, 0, APIFieldComune)
+    else:
+        return "         "
+
+
+def CodProvincia(APIFieldNazione, APIFieldProvincia):
+    if APIFieldNazione.upper() == "ITALIA":
+        return APIFieldProvincia.upper()
+    else:
+        return "  "
+
+def CodRilascio(APIFieldNazione, APIFieldComune):
+    if APIFieldNazione.upper() == "ITALIA":
+        return get_csv(file_CodiceComune, 1, 0, APIFieldComune.upper())
+    else:
+        return get_csv(file_CodiceNazione, 1, 0, APIFieldNazione.upper())
+
 
 def lengthcheck(str):
     print(str)
     print(len(str))
 
 
-string = get_csv('fA', file_TipoAlloggi) + Data('2016/04/25') + NGiorni('01') + Name('Noris', 50) + Name('Matteo', 30) + 'm' + Data('1976/10/31')
+string = get_csv(file_TipoAlloggi,0,1,"FA") + Data('2016/04/25') + NGiorni('01') + Name('Noris', 50) + Name('Matteo', 30) + 'm'.upper() + Data('1976/10/31') + CodComune("ItALIA", "ALMESE") + CodProvincia("Italia", "rm") + get_csv(file_CodiceNazione,1,0,"BHUTAN") + get_csv(file_CodiceNazione,1,0,"ITaLIA") + get_csv(file_CodiceDocumento,1,0,"PASSAPORTO ORDINARIO") + Name("1234567890", 20) + CodRilascio("ITALIA", "ALMESE") + chr(13) + chr(10)
+
 lengthcheck(string)
