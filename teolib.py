6298223668cc36dd54893b1c0f1d6bf6dba1c099
Paymon <matteo.noris@gmail.com>
2016-04-27 13:27:31 +0200

    Nuovo Codice

--- teolib.py
+++ teolib.py
@@ -10,6 +10,50 @@ def file(filename, text):
     f.write(text)
     f.close()
 
+
+def array2file(filename, array):
+    f = open(filename, 'w')
+    for string in array:
+        f.write(string)
+    f.close()
+
+def emailfile(address, object, message, filename):
+    import smtplib
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+    from email.mime.base import MIMEBase
+    from email import encoders
+
+    fromaddr = "matteo.noris@gmail.com"
+    toaddr = "matteo.noris@gmail.com"
+
+    msg = MIMEMultipart()
+
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = ""
+
+    body = "TEXT YOU WANT TO SEND"
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    attachment = open(filename, "rb")
+
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+    msg.attach(part)
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr, "f3d3r1c4!!")
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
+
+
 def get_json(url):
     with request.urlopen(url) as response:
         return json.loads(response.read().decode("utf-8"))
--- toPoliziaDiStato.py
+++ toPoliziaDiStato.py
@@ -1,7 +1,9 @@
 import teolib
+import datetime
 import csv
 
-
+crlf = chr(13) + chr(10)
+vettore = []
 file_TipoAlloggi = "Codici_TipoAlloggiato.csv"
 file_CodiceComune = "Comuni.csv"
 file_CodiceNazione = "Nazioni.csv"
@@ -48,6 +50,7 @@ def CodProvincia(APIFieldNazione, APIFieldProvincia):
     else:
         return "  "
 
+
 def CodRilascio(APIFieldNazione, APIFieldComune):
     if APIFieldNazione.upper() == "ITALIA":
         return get_csv(file_CodiceComune, 1, 0, APIFieldComune.upper())
@@ -60,6 +63,18 @@ def lengthcheck(str):
     print(len(str))
 
 
-string = get_csv(file_TipoAlloggi,0,1,"FA") + Data('2016/04/25') + NGiorni('01') + Name('Noris', 50) + Name('Matteo', 30) + 'm'.upper() + Data('1976/10/31') + CodComune("ItALIA", "ALMESE") + CodProvincia("Italia", "rm") + get_csv(file_CodiceNazione,1,0,"BHUTAN") + get_csv(file_CodiceNazione,1,0,"ITaLIA") + get_csv(file_CodiceDocumento,1,0,"PASSAPORTO ORDINARIO") + Name("1234567890", 20) + CodRilascio("ITALIA", "ALMESE") + chr(13) + chr(10)
+string = get_csv(file_TipoAlloggi,0,1,"FA") + Data('2016/04/25') + NGiorni('01') + Name('Noris', 50) + Name('Matteo', 30) + 'm'.upper() + Data('1976/10/31') + CodComune("ItALIA", "ALMESE") + CodProvincia("Italia", "rm") + get_csv(file_CodiceNazione,1,0,"BHUTAN") + get_csv(file_CodiceNazione,1,0,"ITaLIA") + get_csv(file_CodiceDocumento,1,0,"PASSAPORTO ORDINARIO") + Name("1234567890", 20) + CodRilascio("ITALIA", "ALMESE")
 
 lengthcheck(string)
+
+
+for i in range(0,9):
+    if i != 0:
+        vettore.append(crlf + string)
+    else:
+        vettore.append(string)
+
+exportfilename = "Alloggiati_" + datetime.datetime.now().strftime("%Y-%m-%d") + "txt"
+teolib.array2file(exportfilename, vettore)
+
+#teolib.emailfile("","","",exportfilename)
